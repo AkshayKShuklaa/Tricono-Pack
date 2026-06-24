0eb00221f3de2c59fd870703f54379b3b3c1bd4a
AkshayKShuklaa <2k22.csaiml.32598@gmail.com>
2026-06-24 22:18:02 +0530

    Setup dynamic environment auto-detection for database connection

--- includes/db.php
+++ includes/db.php
@@ -5,10 +5,19 @@
 DATABASE CONFIG
 ========================= */
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "heeral";
+if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1')) {
+    // Local XAMPP Config
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "heeral";
+} else {
+    // Hostinger Production Config
+    $host = "localhost";
+    $user = "u695279284_tricono";
+    $pass = "_*Qq3F8UwvZBv*.";
+    $dbname = "u695279284_tricono";
+}
 
 /* =========================
 CREATE CONNECTION
